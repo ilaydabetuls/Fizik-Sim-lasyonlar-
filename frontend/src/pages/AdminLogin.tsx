cba7bb9ba1f6498e50cf09bde2d477d40fcfdd21
ilaydabetull <ilaydabetul@gmail.com>
2026-07-16 23:52:30 +0300

    chore: add AdminLogin component to fix build

---
+++ frontend/src/pages/AdminLogin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminLogin: React.FC = () => {
+  return (
+    <div>
+      <h1>Admin Login</h1>
+      {/* TODO: Add form fields and authentication logic */}
+    </div>
+  );
+};
+
+export default AdminLogin;
