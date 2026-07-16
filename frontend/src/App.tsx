5763cfe3cff637b0ce0fe83038d204e03c4520b2
İlayda Betül SARAR <ilaydabetulsarar@gmail.com>
2026-07-16 23:27:44 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import PreTest from './pages/PreTest';
 import PostTest from './pages/PostTest';
+import AdminDashboard from './pages/AdminDashboard';
+import ParticipantDetail from './pages/ParticipantDetail';
+import AdvancedAnalysis from './pages/AdvancedAnalysis';
+import AdminLogin from './pages/AdminLogin'; // YENİ EKLENDİ
 
 export default function App() {
   return (
@@ -10,6 +14,13 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/pre-test" element={<PreTest />} />
         <Route path="/post-test" element={<PostTest />} />
+        
+        {/* Yönetici Giriş Rotası */}
+        <Route path="/admin-login" element={<AdminLogin />} /> 
+        
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/participant/:code" element={<ParticipantDetail />} />
+        <Route path="/admin/analysis" element={<AdvancedAnalysis />} />
       </Routes>
     </BrowserRouter>
   );
